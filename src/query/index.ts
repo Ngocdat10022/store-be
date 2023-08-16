2f13b410945e838be181cd01b655ebce6c929d3d
NgocDat1002 <huynhngocdat2002.td@gmail.com>
2023-08-16 23:20:46 +0700

    getall products

--- src/query/index.ts
+++ src/query/index.ts
@@ -3,7 +3,7 @@ export  const getAllUser = `SELECT * FROM category`
 
 // query product
 
-export const getAllProduct = `SELECT * FROM products`
+export const getAllProduct = `SELECT PP.id, PP.name,PP.slug, PP.initial_price,PP.discounted_price,PP.discount_percentage, P.imageUrl FROM parents_product as PP INNER JOIN products AS P ON PP.id = P.parentProductsId `
 
 // query user
 export const getUserEmail = `SELECT * from user WHERE email=? `
